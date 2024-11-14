08668eeb75acb72a98f4d93fe476ef31697573d9
J-Breno <joao.breno85@hotmail.com>
2024-11-13 23:32:49 -0300

    Iniciando projeto em tailwindCss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.tsx",
   ],
   theme: {
     extend: {
+      maxWidth: {
+        app: '700px'
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        breno: '#8257e6'
       },
     },
   },
