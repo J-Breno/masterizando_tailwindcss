07c98d02a08b96b5fff64f1e47ed74b7db3a37e8
J-Breno <joao.breno85@hotmail.com>
2024-11-14 02:57:28 -0300

    Terminando parte inicial da sideBar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
+      },
       maxWidth: {
-        app: '700px'
+        app: '700px',
       },
       colors: {
-        breno: '#8257e6'
+        breno: '#8257e6',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
